c5c9ed1815850ca341d64b84f77b7528f80ab30c
Ameen Syed <asyed08@gmail.com>
2019-10-27 21:21:35 -0400

    Feature: Added addition Unit Test, addition CSV, addition functionality

--- src/Calculator.py
+++ src/Calculator.py
@@ -6,12 +6,21 @@ def subtraction(a, b):
     c = b - a
     return c
 
+def addition(a, b):
+    a = int(a)
+    b = int(b)
+    c = a + b
+    return c
+
 class Calculator:
     result = 0
 
     def __init__(self):
         pass
 
+    def add(self, a, b):
+        self.result = addition(a, b)
+        return self.result
 
     def subtract(self, a, b):
         self.result = subtraction(a, b)
