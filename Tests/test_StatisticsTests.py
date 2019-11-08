8f47e6ddf9a0a61f0bfdb3443ddcc4478ee219ca
Ameen Syed <asyed08@gmail.com>
2019-11-08 16:21:41 -0500

    Fix: Added stats data & fixed stats tests

--- Tests/test_StatisticsTests.py
+++ Tests/test_StatisticsTests.py
@@ -5,7 +5,7 @@ import pprint
 
 class MyTestCase(unittest.TestCase):
     def setUp(self) -> None:
-        self.statistics = Statistics()
+        self.statistics = Statistics('Tests/Data/PopulationData.csv')
 
     def test_instantiate_statscalculator(self):
         self.assertIsInstance(self.statistics, Statistics)
