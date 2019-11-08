cc8889ed35a5b6c544d685c1612d4612df23e90d
Ameen Syed <asyed08@gmail.com>
2019-11-08 16:52:58 -0500

    Fix: Corrected Stats Tests

--- Tests/test_StatisticsTests.py
+++ Tests/test_StatisticsTests.py
@@ -9,6 +9,7 @@ class MyTestCase(unittest.TestCase):
     def setUp(self) -> None:
         self.statistics = Statistics('Tests/Data/PopulationData.csv')
 
+
     def test_instantiate_statscalculator(self):
         self.assertIsInstance(self.statistics, Statistics)
 
@@ -16,8 +17,8 @@ class MyTestCase(unittest.TestCase):
         test_data = CsvReader('Tests/Data/PopulationData.csv').data
         answers = CsvReader('Tests/Data/StatsAnswers.csv').data
         for column in test_data:
-            result = self.answers(float(column['Population Mean']))
-            self.assertEqual(self.statistics.popmean['Number'], result)
+            result = answers(float(column['Population Mean']))
+            self.assertEqual(self.test_data.popmean['Number'], result)
 
 
 if __name__ == '__main__':
