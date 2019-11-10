cf209051c0168905c56ef38023648d909cbbd142
Ameen Syed <asyed08@gmail.com>
2019-11-10 14:21:29 -0500

    Fix: Corrected Stats Tests

--- Tests/test_StatisticsTests.py
+++ Tests/test_StatisticsTests.py
@@ -16,8 +16,13 @@ class MyTestCase(unittest.TestCase):
 
     def test_pop_mean_statistics(self):
         answers = CsvReader('Tests/Data/StatsAnswers.csv').data
+        test_data = CsvReader('Tests/Data/PopulationData.csv').data
+        pprint(answers)
+        pprint(test_data)
+        pprint(self.statistics.popmean(test_data))
         for row in answers:
-            self.assertEqual(self.statistics.popmean(self),int(row['Population Mean']))
+            pprint(int(row['Population Mean']))
+            self.assertEqual(self.statistics.popmean(test_data), int(row['Population Mean']))
 
 
 if __name__ == '__main__':
