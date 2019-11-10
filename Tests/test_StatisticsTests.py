b50f54255e3dc8e9895b94f56c31b4c30e3b328a
Ameen Syed <asyed08@gmail.com>
2019-11-10 15:45:00 -0500

    Feature: Added Sample Mean functionality, unit test, & csv

--- Tests/test_StatisticsTests.py
+++ Tests/test_StatisticsTests.py
@@ -21,5 +21,12 @@ class MyTestCase(unittest.TestCase):
             self.assertEqual(self.statistics.popmean(row['Num1'], row['Num2'], row['Num3']), answer)
             self.assertEqual(self.statistics.result, answer)
 
+    def test_samp_mean_stats(self):
+        test_data = CsvReader('Tests/Data/SampleMean.csv').data
+        for row in test_data:
+            answer = float(row['Answer'])
+            self.assertEqual(self.statistics.sampmean(row['SampleSize'],row['Num1'],row['Num2'],row['Num3'],row['Num4'],row['Num5']))
+            self.assertEqual(self.statistics.result, answer)
+
 if __name__ == '__main__':
     unittest.main()
