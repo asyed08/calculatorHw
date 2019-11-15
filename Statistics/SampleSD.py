970dbb39e3d0e12cf27138c8866fbbe57326a511
Ameen Syed <asyed08@gmail.com>
2019-11-15 11:10:11 -0500

    Added Sample Standard Deviation functionality

---
+++ Statistics/SampleSD.py
@@ -0,0 +1,30 @@
+from Statistics.PopulationSD import population_standard_deviation
+
+def sample_standard_deviation(samplesize,a,b,c,d,e,f,g,h):
+    try:
+        a = float(a)
+        b = float(b)
+        c = float(c)
+        d = float(d)
+        e = float(e)
+        f = float(f)
+        g = float(g)
+        h = float(h)
+        samplesize = int(samplesize)
+        data = []
+        data.append(a)
+        data.append(b)
+        data.append(c)
+        data.append(d)
+        data.append(e)
+        data.append(f)
+        data.append(g)
+        data.append(h)
+        sample = []
+        for i in range(0,samplesize-1):
+            sample.append(int(data[i]))
+        return population_standard_deviation(sample[0],sample[1],sample[2],sample[3],sample[4],sample[5])
+    except ZeroDivisionError:
+       print("Cannot divide by zero")
+    except ValueError:
+        print("Numbers are not valid")
