78ed73327c520e441ae647d4c07f75b3604e7159
Ameen Syed <asyed08@gmail.com>
2019-11-15 17:31:15 -0500

    Fixed Defect

--- Statistics/statistics.py
+++ Statistics/statistics.py
@@ -8,6 +8,8 @@ from Statistics.PopulationSD import population_standard_deviation
 from Statistics.SampleSD import sample_standard_deviation
 from Statistics.zscore import zscore
 from pprint import pprint
+
+
 class Statistics(Calculator):
     def __init__(self):
         super().__init__()
@@ -22,25 +24,25 @@ class Statistics(Calculator):
         return self.result
 
     def median(self, a, b, c, d, e, f, g):
-        self.result = median(a,b,c,d,e,f,g)
+        self.result = median(a, b, c, d, e, f, g)
         return self.result
 
-    def pop_variance(self, a, b, c, d, e, f,):
+    def pop_variance(self, a, b, c, d, e, f, ):
         self.result = population_variance(a, b, c, d, e, f)
         return self.result
 
     def population_standard_deviation(self, a, b, c, d, e, f):
         self.result = population_standard_deviation(a, b, c, d, e, f)
         return self.result
-      
+
     def mode(self, a, b, c, d, e):
         self.result = mode(a, b, c, d, e)
         return self.result
 
-    def sample_standard_deviation(self,samplesize,a,b,c,d,e,f,g,h):
-        self.result = sample_standard_deviation(samplesize,a,b,c,d,e,f,g,h)
+    def sample_standard_deviation(self, samplesize, a, b, c, d, e, f, g, h):
+        self.result = sample_standard_deviation(samplesize, a, b, c, d, e, f, g, h)
         return self.result
 
-    def zscore(self, datapoint, a,b,c,d,e,f):
-        self.result = zscore(datapoint,a,b,c,d,e,f)
+    def zscore(self, datapoint, a, b, c, d, e, f):
+        self.result = zscore(datapoint, a, b, c, d, e, f)
         return self.result
