a7ead3a784fcb7ac24767b897722067b7b81b8f7
Ameen Syed <asyed08@gmail.com>
2019-11-14 21:08:57 -0500

    Fixed Defect

--- Statistics/PopulationVar.py
+++ Statistics/PopulationVar.py
@@ -4,7 +4,7 @@ from Calculator.square import square
 def population_variance(a, b, c, d, e, f):
     try:
         popstd = population_standard_deviation(a,b,c,d,e,f)
-        answer = square(popstd)
+        answer = float(square(popstd))
         return answer
     except ZeroDivisionError:
         print("Cannot divide by zero")
